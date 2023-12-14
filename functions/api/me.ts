c607716ffbe55ff3bdf871200d5865de0e23b1e2
satoshi toyama <satoshi.toyama@route06.co.jp>
2023-12-14 22:15:33 +0900

    me

--- functions/api/me.ts
+++ functions/api/me.ts
@@ -1,9 +1,20 @@
 import type { PluginData } from "@cloudflare/pages-plugin-cloudflare-access";
+import { drizzle } from "drizzle-orm/d1";
+import * as schema from "../../db/schema";
 import { Env } from "../env";
-export const onRequestGet: PagesFunction<Env, string, PluginData> = ({
+export const onRequestGet: PagesFunction<Env, string, PluginData> = async ({
+	env,
 	data,
 }) => {
-	return new Response(JSON.stringify(data), {
+	const db = drizzle(env.DB, { schema });
+	const user = await db.query.users.findFirst({
+		where: (users, { eq }) =>
+			eq(users.email, data.cloudflareAccess.JWT.payload.email ?? "noemail"),
+	});
+	if (user == null) {
+		return new Response("no user found", { status: 404 });
+	}
+	return new Response(JSON.stringify(user), {
 		headers: {
 			"content-type": "application/json",
 		},
