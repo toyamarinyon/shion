b8925f612d120083d8945280e1e4a07eb3fd5d4a
satoshi toyama <satoshi.toyama@route06.co.jp>
2023-12-14 22:15:35 +0900

    local

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -4,6 +4,13 @@ import { Env } from "./env";
 export const onRequest: PagesFunction<Env> = (context) => {
 	const url = new URL(context.request.url);
 	if (url.hostname === "localhost" || url.hostname === "127.0.0.1") {
+		context.data.cloudflareAccess = {
+			JWT: {
+				payload: {
+					email: "toyamarinyon@gmail.com",
+				},
+			},
+		};
 		return context.next();
 	}
 	return cloudflareAccessPlugin({
